361f2b27970e317d44a691a87798fc30ec519e43
sfmajors373 <sfmajors373@gmail.com>
2021-09-09 13:54:13 -0400

    Debug statements added

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,8 +10,10 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    std::cout << "Before setting start and end nodes\n";
 	  *start_node = m_Model.FindClosestNode(start_x, start_y);
     *end_node = m_Model.FindClosestNode(end_x, end_y);
+    std::cout << "After setting start and end nodes\n";
 }
 
 
